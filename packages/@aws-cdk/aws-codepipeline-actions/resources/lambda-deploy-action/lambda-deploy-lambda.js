763a2d8dd56aa7802fc33263fa5bc9b87a9df11d
Adam Ruka <adamruka@amazon.com>
2019-04-09 16:06:30 -0700

    Do an S3 request to retrieve the input artifact.

--- packages/@aws-cdk/aws-codepipeline-actions/resources/lambda-deploy-action/lambda-deploy-lambda.js
+++ packages/@aws-cdk/aws-codepipeline-actions/resources/lambda-deploy-action/lambda-deploy-lambda.js
@@ -3,20 +3,41 @@ var AWS = require('aws-sdk');
 exports.handler = function (event, context) {
     console.info("Called with Event: " + JSON.stringify(event, null, 2));
 
-    var codepipeline = new AWS.CodePipeline();
+    const pipelineEvent = event['CodePipeline.job'];
+    const pipelineData = pipelineEvent['data'];
 
-    // Retrieve the Job ID from the Lambda action
-    var jobId = event['CodePipeline.job'].id;
+    const inputs = pipelineData['inputArtifacts'];
+    if (inputs.length > 0) {
+        const s3Location = inputs[0]['location']['s3Location'];
+        const s3 = new AWS.S3({
+            ...pipelineData.artifactCredentials,
+            params: {
+                Bucket: s3Location.bucketName,
+            },
+        });
+        s3.getObject({
+            Key: s3Location.objectKey,
+        }, function (err, data) {
+            if (err) {
+                console.error(err, err.stack);
+            } else {
+                console.log(`S3 data: ${data}`);
+            }
+        });
+    }
 
-    // Notify AWS CodePipeline of a successful job
-    var params = {
+    callPipelineApi(pipelineEvent.id, context);
+};
+
+function callPipelineApi(jobId, context) {
+    const codepipeline = new AWS.CodePipeline();
+    codepipeline.putJobSuccessResult({
         jobId: jobId,
-    };
-    codepipeline.putJobSuccessResult(params, function (err, data) {
+    }, function (err) {
         if (err) {
             context.fail(err);
         } else {
             context.succeed('Hello from Lambda!');
         }
     });
-};
+}
